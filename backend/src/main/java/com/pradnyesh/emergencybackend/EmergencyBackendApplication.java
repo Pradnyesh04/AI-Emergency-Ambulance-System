fda1a940c66734b0ef70e9347b762f744ae2907f
Pradnyesh04 <pradnyeshwork4@gmail.com>
2026-08-12 13:35:07 +0530

    Initialize emergency ambulance backend

---
+++ backend/src/main/java/com/pradnyesh/emergencybackend/EmergencyBackendApplication.java
@@ -0,0 +1,12 @@
+package com.pradnyesh.emergencybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyBackendApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/pradnyesh/emergencybackend/EmergencyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradnyesh.emergencybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
